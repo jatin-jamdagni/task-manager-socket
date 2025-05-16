b0fe74624ff3170459b060f93cac1898bae3547c
Jatin Sharma <jatinsharma13arril2002@gmail.com>
2025-05-16 14:43:24 +0530

    Deployement 2025-05-16 vercel

---
+++ start-prod.sh
@@ -0,0 +1,13 @@
+echo "Building client app..."
+cd client
+npm install
+npm run build
+
+cd ..
+
+echo "Installing server dependencies..."
+cd server
+npm install
+
+echo "Starting Express server in production..."
+node index.js
